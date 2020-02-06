c145d1ee6b5a7d4955bfc747aa1f4e601649381a
Blake Tarter <blakertarter@gmail.com>
2020-02-06 16:08:30 -0600

    Update Storybook setup and add loki

--- .storybook/index.ts
+++ .storybook/index.ts
@@ -1,4 +1,5 @@
 import { getStorybookUI, configure } from "@storybook/react-native"
+import "loki/configure-react-native"
 
 configure(() => {
   require("../src/stories")
@@ -6,4 +7,4 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-export const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true })
+export const StorybookUIRoot = getStorybookUI({ port: 7007, host: 'localhost', onDeviceUI: false })
