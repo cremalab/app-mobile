45b659e799a2ede554b446530e758c03b3f393d3
Blake Tarter <blakertarter@gmail.com>
2020-02-25 12:58:29 -0600

    Check expo start for any errors in CI

---
+++ test-expo-start.sh
@@ -0,0 +1,8 @@
+# This is super hacky..
+# If "npm start" outputs anything to stderr in 5s then return with an exit code
+if timeout --preserve-status 5s npm start 2>&1 >/dev/null | grep -m 1 -c -e '.*' ; then
+  timeout --preserve-status 5s npm start 2>&1 >/dev/null
+  exit 1
+else
+  exit 0
+fi
