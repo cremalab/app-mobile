75434e009b41a3ffc94fa20ef1c779419e49e141
Blake Tarter <blakertarter@gmail.com>
2020-03-10 16:02:10 -0500

    Update test:expo script

--- test-expo-start.sh
+++ test-expo-start.sh
@@ -1,7 +1,7 @@
 # This is super hacky..
 # If "npm start" outputs anything to stderr in 5s then return with an exit code
-if timeout --preserve-status 5s npm start 2>&1 >/dev/null | grep -m 1 -c -e '.*' ; then
-  timeout --preserve-status 5s npm start 2>&1 >/dev/null
+if timeout --preserve-status 5s npm start -- --non-interactive 2>&1 >/dev/null | grep -m 1 -c -e '.*' ; then
+  timeout --preserve-status 5s npm start -- --non-interactive 2>&1 >/dev/null
   exit 1
 else
   exit 0
--- test-expo-watch-mode.sh
+++ test-expo-watch-mode.sh
@@ -1,7 +1,7 @@
 # This is super hacky..
 # If "npm start" outputs "Watch mode is not supported in this environment" in 30s then return with an exit code
-if timeout --preserve-status 30s npm start | grep -m 1 -c -e 'Watch mode is not supported in this environment' ; then
-  timeout --preserve-status 30s npm start
+if timeout --preserve-status 30s npm start -- --non-interactive | grep -m 1 -c -e 'Watch mode is not supported in this environment' ; then
+  timeout --preserve-status 30s npm start -- --non-interactive
   exit 1
 else
   exit 0
