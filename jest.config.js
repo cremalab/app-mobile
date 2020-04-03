c44093779d7ec7ffdc63e6afd35f504d6286a056
Blake Tarter <blakertarter@gmail.com>
2020-04-03 10:13:09 -0500

    Update the jest transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,9 @@ module.exports = Object.assign(expoPreset, jestPreset, {
     "!src/stories/*",
     "src/**/*.{ts,tsx}",
   ],
+  transformIgnorePatterns: [
+    ...expoPreset.transformIgnorePatterns,
+    ...jestPreset.transformIgnorePatterns,
+    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.|react-navigation|@react-navigation/.|@unimodules/.*|unimodules|sentry-expo|native-base)",
+  ],
 })
