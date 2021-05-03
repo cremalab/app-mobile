09a237f1dbbb2c9909c769a5687cfa5081646a84
Rob LaFeve <roblafeve@gmail.com>
2021-05-03 16:01:35 -0500

    Update eslint prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "prettier/@typescript-eslint",
     "react-app",
     "plugin:prettier/recommended",
   ],
